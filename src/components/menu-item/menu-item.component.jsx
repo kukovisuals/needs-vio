c94ac8ec32e5f884fe54a7db5dbda6ded8b873fa
kukovisuals <clubhousenet@live.com>
2020-07-24 11:27:51 -0400

    update our menu items to navigate with router HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
 
-	<div className={`${size} menu_item`} > 
+	<div 
+		className={`${size} menu_item`} 
+		onClick={() => history.push(`${match.url}${linkUrl}`)} 
+	> 
 		<div className='background-image'
 					style= {{
 						backgroundImage: `url(${imageUrl} )`
 					}}
 			/>
 		<div className='content'> 
-			<h1 className='title'>{title}</h1>
+			<h1 className='title'>{title.toUpperCase()}</h1>
 			<span className='subtitle'> SHOP NOW </span>
 		</div>
 	</div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -3,12 +3,8 @@ import Directory from '../../components/directory/directory.component';
 import './homepage.style.scss';
 
 const HomePage = () => (
-	<div className='homepage'> 
-		<div className='directory_menu'> 
-			
-			<Directory />
-
-		</div>
+	<div className='homepage'> 			
+		<Directory />
 	</div>
 );
 
