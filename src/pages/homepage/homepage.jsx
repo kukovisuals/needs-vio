a4329a91fe006c0cb1034c89555cd74885981c95
kukovisuals <clubhousenet@live.com>
2020-07-24 09:54:46 -0400

    broken our homepage into a smaller component, and style our background images

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+import './homepage.style.scss';
+
+const HomePage = () => (
+	<div className='homepage'> 
+		<div className='directory_menu'> 
+			
+			<Directory />
+
+		</div>
+	</div>
+);
+
+export default HomePage;
