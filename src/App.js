90d67d29e7657ae2f66ee11cee72788c98cd4a51
kukovisuals <clubhousenet@live.com>
2020-07-24 16:32:27 -0400

    created sigin in and sing up page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import signInAndUp from './pages/sign-in-and-up/sign-in-and-up.component';
+import Header   from './components/header/header.component';
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={signInAndUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-up.style.scss';
+
+const signInAndUp = () => (
+	<div className='sign-in-and-up'>
+		<SignIn/>
+	</div>
+);
+
+export default signInAndUp;
