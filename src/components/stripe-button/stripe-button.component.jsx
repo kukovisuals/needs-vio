227aa06da840277b5698ce5a7284e0ae8db8d783
kukovisuals <clubhousenet@live.com>
2020-07-30 14:01:43 -0400

    updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51Gxov8IDQXkFzx4GcD1C1Q9QPFfjWKwku42Lcsx2ZBXA7Ex1uHz69rRp7TaERPAotJJQtV8iB8RRWPma8Ym9QuHS00JczW8PMm';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='UNI slides'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
