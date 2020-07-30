68a0c4d5fb69cc6cd36b56e9f1adaa9521861ca8
kukovisuals <clubhousenet@live.com>
2020-07-29 20:40:34 -0400

    added arrow functionality for quanity in shopping cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.type';
-import { addItemToCart } from './cart.utls';
+import { addItemToCart, removeItemFromCart } from './cart.utls';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -13,11 +13,23 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
-      case CartActionTypes.ADD_ITEM:
-       return {
+    case CartActionTypes.ADD_ITEM:
+      return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
        };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      }
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
     default:
       return state;
   }
